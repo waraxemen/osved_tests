17fe57988a06580df9a1e3827d9254b93680343b
petrov.va <petrov.va@svitok31.ru>
2026-06-20 19:04:17 +0300

    начал заполнять форму создания сотрудника

--- features/steps/all_steps.py
+++ features/steps/all_steps.py
@@ -21,18 +21,16 @@ def wait(context, timeout):
 
 @Given('В поле "{field}" вводим "{value}"') # type: ignore
 def enter_to_field(context, field, value):
-    # Playwright сам ждёт появления элемента и его готовности к вводу
-    locator = f"//label[contains(text(), '{field}')]/following-sibling::div//input"
-    
-    # fill() автоматически очищает поле перед вводом
-    context.page.fill(locator, value)
+    context.page.fill(f"//label[contains(text(), '{field}')]/following-sibling::div//input", value)
 
 @Given('Нажимаем на кнопку "{button}"') # type: ignore
 def click_button(context, button):
-    locator = f"//button[contains(text(), '{button}')]"
-    context.page.click(locator)
+    context.page.click(f"//button[contains(text(), '{button}')]")
 
 @Given('Проверяем что текущий пользователь "{user}"') # type: ignore
 def check_current_user(context, user):
-    locator = f'//header//p[contains(text(), "{user}")]'
-    context.page.wait_for_selector(locator)
+    context.page.wait_for_selector(f'//header//p[contains(text(), "{user}")]')
+
+@Given('Переходим на боковую вкладку "{tab}"') # type: ignore
+def switch_to_tab(context, tab):
+    context.page.click(f'//ul/li//span[contains(text(),"{tab}")]')

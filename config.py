9ae8b143477cfb5705c984def1d463315301eebd
petrov.va <petrov.va@svitok31.ru>
2026-06-21 13:37:02 +0300

    ~

---
+++ config.py
@@ -0,0 +1,13 @@
+# адрес сервера
+IP = "localhost:8095"
+
+# актуальная версия PostgreSQL
+PG_BIN = r"C:\Program Files\PostgreSQL\9.6\bin"
+
+# путь к бэкапу
+DUMP = r"backups/dump-svitok_osved.sql"
+
+# адрес и имя БД
+DB_IP = "localhost"
+DB_PORT = "5432"
+DB_NAME = "svitok_osvedomlennost"
